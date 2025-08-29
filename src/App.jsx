0d3c43fa69f7973a579fab71d989880dfd5aeec7
Elias Derbew <eliasderbew1@gmail.com>
2025-08-30 02:12:54 +0300

    initial setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
 export default function App() {
-  return <div>this is the app component</div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <h1>ToDo List</h1>
+      </div>
+
+      <div>
+        <h3>Your Daily Manager</h3>
+      </div>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter your task here . . ." />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+function Footer() {
+  return <div>
+    <div></div>
+  </div>;
 }
