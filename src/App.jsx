1993d1815798ce61f8e180e58c575294859ddaf4
Elias Derbew <eliasderbew1@gmail.com>
2025-08-31 01:03:42 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div>
+    <div className="h-screen bg-indigo-950 text-white">
       <Header />
       <Main />
       <Footer />
@@ -26,7 +26,7 @@ function Main() {
   return (
     <div>
       <form>
-        <input type="text" placeholder="Enter your task here . . ." />
+        <input className="border" type="text" placeholder="Enter your task here . . ." />
         <button>Add</button>
       </form>
     </div>
@@ -34,7 +34,39 @@ function Main() {
 }
 
 function Footer() {
-  return <div>
-    <div></div>
-  </div>;
+  return (
+    <div className="bg-indigo-700 flex justify-center p-2 gap-4">
+      <a
+        href="https://t.me/Eliasdevstack"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="h-8 transition-transform duration-300 hover:scale-110 hover:rotate-6 cursor-pointer"
+          src="/telegram.png"
+          alt="telegram icon"
+        />
+      </a>
+
+      <a
+        href="https://github.com/EliasDerbewBelay"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="h-8 transition-transform duration-300 hover:scale-110 hover:rotate-6 cursor-pointer"
+          src="/github.png"
+          alt="github icon"
+        />
+      </a>
+
+      <a href="mailto:eliasderbewbly@gmail.com">
+        <img
+          className="h-8 transition-transform duration-300 hover:scale-110 hover:rotate-6 cursor-pointer"
+          src="/email.png"
+          alt="email icon"
+        />
+      </a>
+    </div>
+  );
 }
